e96d2771f01c10f6d22c1931c1c4296a468a0133
hjle2 <02q09q@gmail.com>
2021-03-30 21:33:54 +0900

    AWS EC2, RDS 설정 완료! 주소 저장..ㅎ

--- src/main/java/com/book/springboot/Application.java
+++ src/main/java/com/book/springboot/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-//@EnableJpaAuditing JpaConfig로 이동하여 설정
+//@EnableJpaAuditing JpaConfig로 이동하여 설정.
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
